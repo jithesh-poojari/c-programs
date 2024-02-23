695668770ba0892e48369d838d6a769f95a55bfe
Jithesh <96974839+Jithesh-Poojari@users.noreply.github.com>
2024-02-23 17:09:01 +0530

    fibonacci

---
+++ fibonacci.c
@@ -0,0 +1,14 @@
+// fibonacci
+#include<stdio.h>
+
+int main(){
+    int num1=0, num2=1, n=20, temp;
+   while(num1<20){
+       printf("%d  ",num1);
+      temp=num1;
+      num1=num2;
+      num2=temp+num2;
+   }
+   
+    return 0;
+}
