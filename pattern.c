d70abd42c29468e380a610e9bd6f227504ef7188
Mayur <89019990+Mayur68@users.noreply.github.com>
2024-02-23 18:26:20 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -88,5 +88,36 @@ int main()
     printf("\n");
   }
 
+  //Enter the number of rows: 10
+//          *
+//         * *
+//       *   *
+//       *     *
+//      *       *
+//     *         *
+//   *           *
+//   *             *
+//  *               *
+// *******************
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        for(int k = 1; k <= 2 * i - 1; k++) {
+            if(k == 1 || k == 2 * i - 1 || i == n) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        
+        printf("\n");
+    }
+
   return 0;
 }
