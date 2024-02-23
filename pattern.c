ea004340058411e69a22c218a8b3481b1a5dee57
Jithesh <96974839+Jithesh-Poojari@users.noreply.github.com>
2024-02-23 20:43:09 +0530

    updated triangle hollow pattern

--- pattern.c
+++ pattern.c
@@ -20,6 +20,28 @@ int main()
   }
 
   printf("\n");
+  // *  *  *  *  *
+  // *           *
+  // *           *
+  // *           *
+  // *  *  *  *  *
+  for (i = 0; i < n; i++)
+  {
+    for (j = 0; j < n; j++)
+    {
+      if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+      {
+        printf("*  ");
+      }
+      else
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+
+  printf("\n");
   // *
   // *  *
   // *  *  *
@@ -87,37 +109,86 @@ int main()
     }
     printf("\n");
   }
-
-  //Enter the number of rows: 10
-//          *
-//         * *
-//       *   *
-//       *     *
-//      *       *
-//     *         *
-//   *           *
-//   *             *
-//  *               *
-// *******************
-    int n;
-    printf("Enter the number of rows: ");
-    scanf("%d", &n);
-    
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n - i; j++) {
-            printf(" ");
-        }
-
-        for(int k = 1; k <= 2 * i - 1; k++) {
-            if(k == 1 || k == 2 * i - 1 || i == n) {
-                printf("*");
-            } else {
-                printf(" ");
-            }
-        }
-        
-        printf("\n");
+  printf("\n");
+  //     *
+  //    * *
+  //   * * *
+  //  * * * *
+  // * * * * *
+  for (i = 0; i < n; i++)
+  {
+    for (j = n - 1; j > i; j--)
+    {
+      printf(" ");
     }
+    for (j = 0; j < i + 1; j++)
+    {
+      printf("* ");
+    }
+    printf("\n");
+  }
+
+  printf("\n");
+//     * 
+//    * * 
+//   *   * 
+//  *     * 
+// * * * * *
+  for (i = 0; i < n; i++)
+  {
+    for (j = n - 1; j > i; j--)
+    {
+      printf(" ");
+    }
+    for (j = 0; j < i + 1; j++)
+    {
+      if (i == 0 || i == n - 1 || j == 0 || j == i)
+      {
+        printf("* ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+
+  printf("\n");
+
+  // Enter the number of rows: 10
+  //          *
+  //         * *
+  //       *   *
+  //       *     *
+  //      *       *
+  //     *         *
+  //   *           *
+  //   *             *
+  //  *               *
+  // *******************
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n - i; j++)
+    {
+      printf(" ");
+    }
+
+    for (int k = 1; k <= 2 * i - 1; k++)
+    {
+      if (k == 1 || k == 2 * i - 1 || i == n)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+
+    printf("\n");
+  }
 
   return 0;
 }
