65771ed21f20c718962d3281eff65f847f10b4cb
Jithesh <96974839+Jithesh-Poojari@users.noreply.github.com>
2024-02-23 22:20:37 +0530

    added rhombus shape and updated others

--- pattern.c
+++ pattern.c
@@ -69,7 +69,7 @@ int main()
   {
     for (j = 0; j < i + 1; j++)
     {
-      if (i == 0 || i == n - 1 || j == 0 || j == i)
+      if (i == n - 1 || j == 0 || j == i)
       {
         printf("*  ");
       }
@@ -108,7 +108,7 @@ int main()
   {
     for (j = n; j > i; j--)
     {
-      if (i == 0 || i == n - 1 || j == n || j == i + 1)
+      if (i == 0 || j == n || j == i + 1)
       {
         printf("*  ");
       }
@@ -155,7 +155,7 @@ int main()
     }
     for (j = 0; j < i + 1; j++)
     {
-      if (i == 0 || i == n - 1 || j == 0 || j == i)
+      if (i == n - 1 || j == 0 || j == i)
       {
         printf("*  ");
       }
@@ -201,7 +201,7 @@ int main()
     }
     for (j = n; j > i; j--)
     {
-      if (i == 0 || i == n - 1 || j == n || j == i + 1)
+      if (i == 0 || j == n || j == i + 1)
       {
         printf("*  ");
       }
@@ -247,7 +247,140 @@ int main()
     }
     for (j = 0; j < i + 1; j++)
     {
-      if (i == 0 || i == n - 1 || j == 0 || j == i)
+      if (i == n - 1 || j == 0 || j == i)
+      {
+        printf("* ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+
+  printf("\n");
+
+  // * * * * *
+  //  * * * *
+  //   * * *
+  //    * *
+  //     *
+  for (i = 0; i < n; i++)
+  {
+    for (j = 1; j < i + 1; j++)
+    {
+      printf(" ");
+    }
+    for (j = n; j > i; j--)
+    {
+      printf("* ");
+    }
+    printf("\n");
+  }
+
+  printf("\n");
+
+  // * * * * *
+  //  *     *
+  //   *   *
+  //    * *
+  //     *
+  for (i = 0; i < n; i++)
+  {
+    for (j = 1; j < i + 1; j++)
+    {
+      printf(" ");
+    }
+    for (j = n; j > i; j--)
+    {
+      if (i == 0 || j == n || j == i + 1)
+      {
+        printf("* ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+
+  printf("\n");
+
+  //     *
+  //    * *
+  //   * * *
+  //  * * * *
+  // * * * * *
+  //  * * * *
+  //   * * *
+  //    * *
+  //     *
+  for (i = 0; i < n; i++)
+  {
+    for (j = n - 1; j > i; j--)
+    {
+      printf(" ");
+    }
+    for (j = 0; j < i + 1; j++)
+    {
+      printf("* ");
+    }
+    printf("\n");
+  }
+  for (i = 0; i < n; i++)
+  {
+    for (j = 0; j < i + 1; j++)
+    {
+      printf(" ");
+    }
+    for (j = n - 1; j > i; j--)
+    {
+      printf("* ");
+    }
+    printf("\n");
+  }
+
+  printf("\n");
+
+  //     *
+  //    * *
+  //   *   *
+  //  *     *
+  // *       *
+  //  *     *
+  //   *   *
+  //    * *
+  //     *
+  for (i = 0; i < n; i++)
+  {
+    for (j = n - 1; j > i; j--)
+    {
+      printf(" ");
+    }
+    for (j = 0; j < i + 1; j++)
+    {
+      if (j == 0 || j == i)
+      {
+        printf("* ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  for (i = 0; i < n; i++)
+  {
+    for (j = 0; j < i + 1; j++)
+    {
+      printf(" ");
+    }
+    for (j = n - 1; j > i; j--)
+    {
+      if (j == n - 1 || j == i + 1)
       {
         printf("* ");
       }
