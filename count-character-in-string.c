9793b67b81542354f8007cae40624fb2a04b6c42
Jithesh <96974839+Jithesh-Poojari@users.noreply.github.com>
2024-02-23 17:07:51 +0530

    count character occurence in a string

---
+++ count-character-in-string.c
@@ -0,0 +1,15 @@
+// count character occurence in a string
+#include<stdio.h>
+#include <string.h>
+
+int main(){
+    char str[]="hello", ch='l';
+    int i, count=0;
+    for(i=0; i< strlen(str); i++){
+        if(str[i]==ch){
+            count++;
+        }
+    }
+    printf("Count: %d", count);
+    return 0;
+}
